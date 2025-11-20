50bcd318e8c633c6a9d8db8be77ea69179fd27f6
Mauricio <mauriciorojasanchez@gmail.com>
2025-11-19 21:56:35 -0500

    chore: init project.

---
+++ README.md
@@ -0,0 +1,3 @@
+# sweet_manager
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/sweet_manager/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.sweet_manager
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
